4ddce1aba1166ca5f24ec0c03c9b0ee20c93e910
Josh Young <j22cal@gmail.com>
2021-03-11 17:35:03 -0600

    Add more attributes to Repo class and other cleanup

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@
 
 class IssuesDashboard < Sinatra::Base
   get '/' do
-    @i = IssueList.new(override: false).list
+    @issues = IssueList.new(override: false).list
     erb :index
   end
 end
--- services/repo.rb
+++ services/repo.rb
@@ -1,28 +1,40 @@
 # frozen_string_literal: true
 
 class Repo
-  attr_accessor :url,
+  attr_accessor :name,
+                :description,
+                :url,
                 :tags_url,
+                :languages_url,
+                :commits_url,
+                :issues_url,
+                :created_at,
+                :updated_at,
+                :size,
                 :language,
                 :has_issues,
                 :has_projects,
                 :archived,
                 :open_issues_count,
-                :open_issues,
-                :name,
-                :issues_url
+                :open_issues
 
   def initialize(*args)
+    @name = args.first['name']
+    @description = args.first['description']
     @url = args.first['html_url']
     @tags_url = args.first['tags_url']
+    @languages_url = args.first['languages_url']
+    @commits_url = args.first['commits_url']
+    @issues_url = args.first['issues_url']
+    @created_at = args.first['created_at']
+    @updated_at = args.first['updated_at']
+    @size = args.first['size']
     @language = args.first['language']
     @has_issues = args.first['has_issues']
     @has_projects = args.first['has_projects']
     @archived = args.first['archived']
     @open_issues_count = args.first['open_issues_count']
     @open_issues = args.first['open_issues']
-    @name = args.first['name']
-    @issues_url = args.first['issues_url']
   end
 
   def issue_link
