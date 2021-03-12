1f393935f6f48dbf46cd858310401a4315e16f96
Josh Young <j22cal@gmail.com>
2021-03-11 20:02:54 -0600

    Cleanup attribute setting

--- services/issue.rb
+++ services/issue.rb
@@ -1,15 +1,11 @@
 class Issue
-  attr_accessor :url, :number, :title, :state, :created_at, :updated_at, :author_association, :body
+  ATTRIBUTES = %i(url number title state created_at updated_at author_association body).freeze
+  attr_accessor(*ATTRIBUTES)
 
   def initialize(issue)
-    @url = issue["url"]
-    @number = issue["number"]
-    @title = issue["title"]
-    @state = issue["state"]
-    @created_at = issue["created_at"]
-    @updated_at = issue["updated_at"]
-    @author_association = issue["author_association"]
-    @body = issue["body"]
+    ATTRIBUTES.each do |attr|
+      send("#{attr}=", issue[attr.to_s])
+    end
   end
 
   def user_initiated?
--- services/repo.rb
+++ services/repo.rb
@@ -1,38 +1,13 @@
 class Repo
-  attr_accessor :name,
-                :description,
-                :url,
-                :tags_url,
-                :languages_url,
-                :commits_url,
-                :issues_url,
-                :created_at,
-                :updated_at,
-                :size,
-                :language,
-                :has_issues,
-                :has_projects,
-                :archived,
-                :open_issues_count,
-                :open_issues
+  ATTRIBUTES = %i(name description url tags_url languages_url commits_url issues_url created_at
+                  updated_at size language has_issues has_projects archived open_issues_count
+                  open_issues).freeze
+  attr_accessor(*ATTRIBUTES)
 
   def initialize(*args)
-    @name = args.first['name']
-    @description = args.first['description']
-    @url = args.first['html_url']
-    @tags_url = args.first['tags_url']
-    @languages_url = args.first['languages_url']
-    @commits_url = args.first['commits_url']
-    @issues_url = args.first['issues_url']
-    @created_at = args.first['created_at']
-    @updated_at = args.first['updated_at']
-    @size = args.first['size']
-    @language = args.first['language']
-    @has_issues = args.first['has_issues']
-    @has_projects = args.first['has_projects']
-    @archived = args.first['archived']
-    @open_issues_count = args.first['open_issues_count']
-    @open_issues = args.first['open_issues']
+    ATTRIBUTES.each do |attr|
+      send("#{attr}=", args.first[attr.to_s])
+    end
   end
 
   def issue_link
